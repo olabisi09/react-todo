1dd82d99b927d5eb8d3f5f02b0f04d10e0250ede
olabisiolaoye10 <olabisiolaoye10@yahoo.com>
2022-10-31 12:13:37 +0100

    responsive modal width

--- src/App.tsx
+++ src/App.tsx
@@ -54,7 +54,7 @@ const App: React.FC = () => {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 300,
+    width: {xs: 300, md: 400},
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
