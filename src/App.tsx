9e3379bb65597982820eb2421a7575b102320dec
olabisiolaoye10 <olabisiolaoye10@yahoo.com>
2022-10-31 12:11:35 +0100

    fixed modal width

--- src/App.tsx
+++ src/App.tsx
@@ -54,7 +54,7 @@ const App: React.FC = () => {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: 300,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
